a4bd1365781b7f999d14d73de0e293572f675175
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-04-09 20:33:26 -0700

    heaven is real

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -5,11 +5,11 @@ function WelcomeMessage() {
     <div className="w-3/4 mx-auto  h-[20rem] py-4 flex justify-center items-center">
       <div>
         <div className="w-full text-center text-6xl  font-bold text-sans font-bold bg-gradient-to-r from-blue-700 via-blue-400 to-blue-700  inline-block text-transparent bg-clip-text">
-          Jesus Love you
+          Heaven is Real
         </div>
         <p className="w-full  px-2 md:px-0  xl:w-3/4 text-base text-gray-700 text-center mx-auto py-4">
-          Experience the Love of Jesus at Reach Heaven Church International -
-          You Are Welcome Here!
+          Discover the Reality of Heaven at Reach Heaven Church International -
+          A Place to Experience God's Presence
         </p>
         <div className="flex justify-center">
           <button className="bg-blue-500 text-white font-medium drop-shadow-lg px-4 py-2 rounded-full ">
